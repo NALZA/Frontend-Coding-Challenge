e61d29b4edf00d0d64b635cb129139bb3443a1e2
M Nally <mitchellnally@msn.com>
2021-07-25 21:58:49 +1000

    added final test for user selection

--- client/src/components/__tests__/UserSelection.test.tsx
+++ client/src/components/__tests__/UserSelection.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, getByLabelText, render } from '@testing-library/react';
 import {UserSelection} from '../UserSelection'
 
 describe("renders selection radio buttons", () => {
@@ -8,7 +8,6 @@ describe("renders selection radio buttons", () => {
         const component = render(<UserSelection userType="Manager" onUserTypeChange={handleUserType} /> );
 
         const manager = component.getByTestId("manager")
-        component.getByTestId("admin")
         expect(manager).toHaveAttribute("checked");
     })
     it("renders 2 radio buttons with Admin selected", ()=>{
@@ -17,6 +16,12 @@ describe("renders selection radio buttons", () => {
 
         const admin = component.getByTestId("admin");
         expect(admin).toHaveAttribute("checked");
+    })
+    it('calls "handelUserType" prop on selection', () => {
+        const handleUserType = jest.fn();
+        const component = render(<UserSelection userType="Manager" onUserTypeChange={handleUserType} /> );
+        fireEvent.click(component.getByTestId('admin'));
+        expect(handleUserType).toHaveBeenCalled();
     })
     it("Match Snapshot", ()=>{
         const handleUserType = (event: React.ChangeEvent<HTMLInputElement>) => {};
